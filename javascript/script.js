4a3ae1dd44e6dd891b0a354fb214d6a583ecc6cc
Nadiyah Garris <61765184+NGarris@users.noreply.github.com>
2024-03-21 19:05:37 -0400

    Create script.js

---
+++ javascript/script.js
@@ -0,0 +1,21 @@
+var TxtType = function(el, toRotate, period) {
+  this.toRotate = toRotate;
+  this.el = el;
+  this.loopNum = 0;
+  this.period = parseInt(period, 10) || 2000;
+  this.txt = '';
+  this.tick();
+  this.isDeleting = false;
+};
+
+TxtType.prototype.tick = function() {
+  var i = this.loopNum % this.toRotate.length;
+  var fullTxT = this.toRotate[i];
+
+  if (this.isDeleting) {
+    this.txt = fullTxt.substring(0, this.txt.length - 1);
+  } else {
+    this.txt = fullTxt.substring(0, this.txt.length + 1);
+  }
+
+  this.el.innerHTML
